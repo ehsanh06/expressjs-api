55e9fb936b8af93cf14d7ab78faa19e66796a9b3
E-Hussain06 <ehsanhussain991@gmail.com>
2017-04-12 15:14:54 +0100

    Functional API built

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ let mongoose = require('mongoose');
 
 let config = require('./config');
 let setupController = require('./controllers/setupController');
+let apiController = require('./controllers/apiController');
 
 let app = express();
 let port = process.env.PORT || 3000;
@@ -14,5 +15,6 @@ app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
 setupController(app);
+apiController(app);
 
 app.listen(port);
