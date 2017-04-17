fc55060e3a04b5614c8aa428ed9128c527aed8cb
E-Hussain06 <ehsanhussain991@gmail.com>
2017-04-17 11:18:41 +0100

    built simple front end - to improve

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ let apiController = require('./controllers/apiController');
 let app = express();
 let port = process.env.PORT || 3000;
 
-app.use ('/assets', express.static(__dirname + '/public'));
+app.use ('/', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
